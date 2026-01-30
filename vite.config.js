6b58e2ce3d2cfd1d8ce670deb626296b09194ecc
Tu Nombre <tu.email@example.com>
2026-01-30 16:23:51 -0500

    Ultima Prueba

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
-        proxy: {
-            '/api': 'http://localhost:3000'
-        }
+        // Proxy not needed for Firebase
     },
     // Use relative base path so it works even if not at root domain (sometimes helps with local viewing)
     base: './',
